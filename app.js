29c428ccce132ce169397d0347a1d65671b992bf
theAleXX <th3alexx@gmail.com>
2026-05-18 17:19:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const albums = [
+  {
+    title: 'Clann Zú',
+    year: '(2000)',
+    img: './images/001.jpg',
+  },
+  {
+    title: 'Rua',
+    year: '(2002)',
+    img: './images/002.jpg',
+  },
+  {
+    title: 'Black Coats & Bandages',
+    year: '(2004)',
+    img: './images/003.jpg',
+  },
+];
